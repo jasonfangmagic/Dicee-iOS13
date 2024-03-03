6c8a4a82ce1b63733ae00f194b9e5de581bb5657
jasonfangmagic <jasonfangmagic@gmail.com>
2024-03-03 15:45:15 -0500

    add two images

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,9 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceThree")
+
+        
     }
 
 
